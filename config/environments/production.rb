fe7a087fd3e22f53af30308e0f2065be3907bbbb
mario-bros <marfreru@gmail.com>
2025-08-11 12:19:31 +0700

    added allowed host in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow Railway's generated domain
+  config.hosts << "pet-finder-production.up.railway.app"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
